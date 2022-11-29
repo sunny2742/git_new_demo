db6c1160124b079604b1b222f3cfa2cd7c19c1be
unknown <sunnygogawale0@gmail.com>
2022-11-29 20:06:44 +0530

    area of triangle

--- Area of triangle1.c
+++ Area of triangle1.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+	float area,base,height;
+	printf("\nenter a base and height:");
+	scanf("\n%f%f",&base,&height);
+	area=0.5*base*height;
+	printf("area of triangle:%0.2f",area);
+	
+	
+}
