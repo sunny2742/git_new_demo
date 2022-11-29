74123797bbf3600b2bdd22d0acd77cdd78734c88
unknown <sunnygogawale0@gmail.com>
2022-11-29 12:56:01 +0530

    print 5 t0 1 no

---
+++ 5 to 1 no print.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int i,j;
+	for(i=5;i<=1;i--)
+	{
+//	for(j=1;j<=i;j++)
+//	{
+	   //printf("%d",i*j);	
+	  printf("*");
+	  
+//	}
+	     printf("\n%d",i);
+		 	
+	}
+	
+	return 0;
+}
